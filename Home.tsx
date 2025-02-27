f7708cd399e1ab01443d0705e788381697e3423d
crlapples <crlapples19@gmail.com>
2025-02-26 18:59:24 -0800

    Create Home.tsx

---
+++ Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from "./Home.module.css";
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "",
+  description: ""
+}
+
+const Home: React.FC = () -> {
+  
+  return (
+    
+  );
+};
