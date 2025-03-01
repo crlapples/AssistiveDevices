4948f5ded27f43c88b494b5c8783abe6e82ff672
crlapples <crlapples19@gmail.com>
2025-03-01 09:51:29 -0800

    Update ProductMData.ts

--- ProductMData.ts
+++ ProductMData.ts
@@ -1,4 +1,26 @@
 export const productMData {
+  Walker: {
+    "@type": "Product",
+    name: "Dakseon Mobility Walker",
+    image: "",
+    description: "Lightweight, foldable walker designed for comfort, stability, and mobility support.",
+    brand: {
+      "@type": "Brand",
+      name: "Dakseon"
+    },
+    sku: "",
+    offers: {
+      "@type": "Offer",
+      url: "",
+      priceCurrency: "USD",
+      price: "",
+      availability: "https://schema.org/InStock",
+      seller: {
+        "@type": "Organization",
+        name: "Dakseon"
+      }
+    }
+  },
   ShowerSeat: {
     "@type": "Product",
     name: "Dakseon Bathroom Safety Seat",
