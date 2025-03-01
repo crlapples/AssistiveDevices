6a26dc3e5a46aa43e718f9dae97f51cad30d1201
crlapples <crlapples19@gmail.com>
2025-03-01 07:29:06 -0800

    Update metadata.tsx

--- metadata.tsx
+++ metadata.tsx
@@ -1,6 +1,10 @@
 export const metadata: Metadata = {
   title: "GoAid Foldable Walker - Lightweight & Portable Mobility Aid",
-  description: "Stay mobile with the GoAid Foldable Walker. Sturdy, lightweight, and easy to fold for storage and transport. Designed for stability and ease of use.",
+  description: "Stay mobile with the GoAid Foldable Walker. Sturdy, lightweight, and easy to fold for storage and transport. Designed for stability and ease of use."
+  other: {
+    keywords:
+      "walker, wheeled walker, foldable walker, walking frame, E0141, E0143, E0149, durable medical equipment, DME, mobility aid",
+  },
   openGraph: {
     title: "GoAid Foldable Walker - Lightweight & Portable Mobility Aid",
     description: "A reliable foldable walker designed for comfort and stability. Features an aluminum frame, easy folding mechanism, and non-slip rubber feet.",
