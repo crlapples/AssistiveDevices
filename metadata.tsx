2d961f209bfd158027c24383eea178181b03cf75
crlapples <crlapples19@gmail.com>
2025-03-01 08:12:53 -0800

    Create metadata.tsx

---
+++ metadata.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import type { Metadata } from 'next';
+import { mData } from "./mData.ts";
+import Image from 'next/image';
+
+interface metadataProps {
+  params: {
+    pageName: string;
+  }
+}
+
+interface Image {
+  url: string;
+  width: number;
+  height: number;
+  alt: string;
+}
+
+interface MData {
+  title: string;
+  description: string;
+  other: {
+    keywords: string;
+  };
+  openGraph: {
+    title: string;
+    description: string;
+    url: string;
+    type: string;
+    images: Image[];
+  }
+}
+
+export function generateStaticParams() {
+  return Object.keys(mData).map((pageName) ({
+    pageName
+  }));
+}
+
+export const metadata: Metadata = ({ params }: { metadataProps }) => {
+  const { pageName } = params;
+  const meta = mData[pageName as keyof typeof mData];
+  {
+    title: meta.title,
+    description: meta.description,
+    other: {
+      keywords:
+        meta.other.keywords
+    },
+    openGraph: {
+      title: meta.openGraph.title,
+      description: meta.openGraph.description,
+      url: meta.openGraph.url,
+      type: meta.openGraph.type,
+      images: meta.openGraph.images
+    },
+  };
