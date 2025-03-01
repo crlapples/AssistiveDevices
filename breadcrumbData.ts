21eeaea6539174621e51f5c8511f953693fd9e9e
crlapples <crlapples19@gmail.com>
2025-03-01 10:19:56 -0800

    Update breadcrumbData.ts

--- breadcrumbData.ts
+++ breadcrumbData.ts
@@ -1,1 +1,36 @@
-
+export breadcrumbMData {
+  Walker: {
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Home",
+        "item": "https://yourwebsite.com"
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "Walker",
+        "item": ""
+      }
+    ]
+  },
+  ShowerSeat: {
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Home",
+        "item": "https://yourwebsite.com"
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "Shower Seat",
+        "item": ""
+      }
+    ]
+  }
+};
