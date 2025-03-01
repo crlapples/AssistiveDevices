975433ece0353de553cdbe8ad45c21a1be733192
crlapples <crlapples19@gmail.com>
2025-03-01 09:48:52 -0800

    Update ProductMData.ts

--- ProductMData.ts
+++ ProductMData.ts
@@ -1,1 +1,24 @@
-
+export const productMData {
+  ShowerSeat: {
+    "@type": "Product",
+    name: "Dakseon Bathroom Safety Seat",
+    image: "",
+    description: "Secure and comfortable shower seat with a stable design for added bathroom safety.",
+    brand: {
+      "@type": "Brand",
+      name: "Dakseon"
+    },
+    sku: "",
+    offers: {
+      "@type": "Offer",
+      url: "",
+      priceCurrency: "USD",
+      price: "",
+      availability: "https://schema.org/InStock",
+      seller: {
+        "@type": "Organization",
+        name: "Dakseon"
+      }
+    }
+  }
+};
