ae01983505dc5b485387e78c2127ae11f644de7a
crlapples <crlapples19@gmail.com>
2025-03-01 09:05:35 -0800

    Update ProductMetadata.tsx

--- ProductMetadata.tsx
+++ ProductMetadata.tsx
@@ -1,1 +1,36 @@
+import React from 'react';
+import { productMData } from "./ProductMData";
+import Image from 'next/image';
+
+interface ProductMetadataProps {
+  params: {
+    productName: string;
+  }
+}
+
+interface Image {
+  url: string;
+  width: number;
+  height: number;
+  alt: string;
+}
+
+interface ProductMetaData {
+  title: string;
+  description: string;
+  keywords: string[];
+  openGraph: {
+    title: string;
+    description: string;
+    url: string;
+    type: string;
+    images: Image[];
+  }
+}
+
+export function generateStaticParams() {
+  return Object.keys(productMData).map((productName) => ({
+    productName
+  }));
+}
 
