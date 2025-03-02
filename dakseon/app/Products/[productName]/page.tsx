657727e22fdc5ded058a849e1e3cdaa1bdf4956b
crlapples <crlapples19@gmail.com>
2025-03-02 14:14:52 -0800

    Update page.tsx

--- dakseon/app/Products/[productName]/page.tsx
+++ dakseon/app/Products/[productName]/page.tsx
@@ -131,6 +131,22 @@ const ProductPage = ({ params }: ProductPageProps ) => {
           <p><strong>{product.q2}</strong></p>
           <p>{product.a2}</p>
         </div>
+        <footer className={styles.footer}>
+          <div className={styles.verticalOriA}>
+            <strong>
+              <p>Return to Top</p>
+            </strong>
+            <Image src="/wheelchair-icon.png" width={512} height={512} alt="" className={styles.wheelchairIcon} />
+            <p>© Copyright</p>
+          </div>
+          <div className={styles.verticalOriB}>
+            <div className={styles.horizontalOri}>
+              <Image src="/instagram.1024x1024.png" width={512} height={512} alt="" />
+              <Image src="/logo-black.png" width={512} height={512} alt="" className={styles.x} />
+              <Image src="/tiktok.946x1024.png" width={512} height={512} alt="" />
+            </div>
+          </div>
+        </footer>
       </div>
     </>
   );
