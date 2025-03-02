0e895130ef43d5f365683017284b4df916e31ded
crlapples <crlapples19@gmail.com>
2025-03-01 16:59:39 -0800

    Update Products.tsx

--- Products.tsx
+++ Products.tsx
@@ -131,6 +131,26 @@ const ProductPage = ({ params }: ProductPageProps ) => {
           <p><strong>{product.q2}</strong></p>
           <p>{product.a2}</p>
         </div>
+        <footer className={styles.footer}>
+          <div className={styles.verticalOriA}>
+            <strong>
+              <p>Return to Top</p>
+            </strong>
+            <Image src="/wheelchair-icon.png" width={512} height={512} alt="" className={styles.wheelchairIcon} />
+            <p>Copyright</p>
+          </div>
+          <div className={styles.verticalOriB}>
+            <div className={styles.horizontalOri}>
+              <Image src="/instagram.1024x1024.png" width={512} height={512} alt="" />
+              <Image src="/logo-black.png" width={512} height={512} alt="" className={styles.x} />
+              <Image src="/tiktok.946x1024.png" width={512} height={512} alt="" />
+            </div>
+            <div className={styles.attributions}>
+              <a href="https://www.flaticon.com/free-icons/invalid" title="invalid icons">Invalid icons created by Freepik - Flaticon</a>
+              <a href="https://www.flaticon.com/free-icons/invalid" title="invalid icons">Invalid icons created by edt.im - Flaticon</a>
+            </div>
+          </div>
+        </footer>
       </div>
     </>
   );
