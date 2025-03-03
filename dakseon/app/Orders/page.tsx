a33a388b9d4aafed3a07dcb98ecc6fcd21a0d81e
crlapples <crlapples19@gmail.com>
2025-03-03 09:11:16 -0800

    Update page.tsx

--- dakseon/app/Orders/page.tsx
+++ dakseon/app/Orders/page.tsx
@@ -8,8 +8,8 @@ import Dropdown from "./Dropdown";
 
 const Orders = () => {
   const entrance = useState<boolean>(true);
-  const addWalker = useState<boolean>(false);
-  const addSeat = useState<boolean>(false);
+  const addedWalker = useState<boolean>(false);
+  const addedSeat = useState<boolean>(false);
   const walkerQuantity = useState<number>(0);
   const seatQuantity = useState<number>(0);
   const totalPrice = useState<number>(0);
@@ -19,31 +19,34 @@ const Orders = () => {
   return (
     <div className={styles.mainContainer}>
       (entrance && {
-        
+        <
       })
-      (addWalker || addSeat && {
-        <div className={styles.itemsContainer>
-          (addWalker && {
-            <div className={styles.itemHoriz}>
-              <Image src="" alt="" width={902} height={1600} className={styles.itemImage} />
-              <div className={styles.itemInfo>
-                <p className={styles.itemName}></p>
+      (selectedCart && {
+        
+        (addedWalker || addedSeat && {
+          <div className={styles.itemsContainer>
+            (addedWalker && {
+              <div className={styles.itemHoriz}>
+                <Image src="" alt="" width={902} height={1600} className={styles.itemImage} />
+                <div className={styles.itemInfo>
+                  <p className={styles.itemName}></p>
+                </div>
               </div>
-            </div>
-          })
-          (addSeat && {
-            <div className={styles.itemHoriz}>
-              <Image src="" alt="" width={902} height={1600} className={styles.itemImage} />
-              <div className={styles.itemInfo>
-                <p className={styles.itemName}></p>
+            })
+            (addedSeat && {
+              <div className={styles.itemHoriz}>
+                <Image src="" alt="" width={902} height={1600} className={styles.itemImage} />
+                <div className={styles.itemInfo>
+                  <p className={styles.itemName}></p>
+                </div>
               </div>
+            })
+            <div className={styles.priceContainer}>
+              <p className={styles.totalPrice}>{totalPrice.toFixed(2)}</p>
             </div>
-          })
-          <div className={styles.priceContainer}>
-            <p className={styles.totalPrice}>{totalPrice.toFixed(2)}</p>
+            <div className={styles.
           </div>
-          <div className={styles.
-        </div>
+        })
       })
     </div>
   );
