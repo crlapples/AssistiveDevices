46c315d25ca6f792a23943b9a392159260d53cce
crlapples <crlapples19@gmail.com>
2025-03-02 16:15:29 -0800

    Update About.tsx

--- dakseon/app/About/About.tsx
+++ dakseon/app/About/About.tsx
@@ -13,7 +13,7 @@ const About: React.FC = () => {
         <Dropdown />
       </div>
       <h1>About Dakseon</h1>
-      <p>At Dakseon, we are committed to providing high-quality mobility and safety products that help individuals live with greater independence and confidence.</p>
+      <p className={styles.p}>At Dakseon, we are committed to providing high-quality mobility and safety products that help individuals live with greater independence and confidence.</p>
       
       {/* Image of happy seniors using mobility aids */}
       <div className={styles.imageContainer}>
@@ -27,13 +27,13 @@ const About: React.FC = () => {
       </div>
 
       <h2>Our Story</h2>
-      <p>With a decade of experience in the assistive device industry, we understand the importance of reliable and well-designed products. Our carefully selected mobility and bathroom safety solutions are built to enhance everyday life, ensuring both safety and comfort.</p>
+      <p className={styles.p}>With a decade of experience in the assistive device industry, we understand the importance of reliable and well-designed products. Our carefully selected mobility and bathroom safety solutions are built to enhance everyday life, ensuring both safety and comfort.</p>
 
       <h2 className={styles.secondh2}>Our Commitment</h2>
       <p className={styles.thirdp}>We believe that everyone deserves to move freely and feel secure in their home. Our products are designed to offer durability, ease of use, and peace of mind for individuals and caregivers alike.</p>
 
       <h2>Need Help?</h2>
-      <p>Have questions about our products? Contact our team <Link href="/contact">here</Link>, and we’ll be happy to assist you.</p>
+      <p className={styles.p}>Have questions about our products? Contact our team <Link href="/contact">here</Link>, and we’ll be happy to assist you.</p>
       <footer className={styles.footer}>
           <div className={styles.verticalOriA}>
             <strong>
